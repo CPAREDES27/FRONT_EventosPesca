3c9b20a8409118a263278a0d9f8616d022a1c2ea
amagno962021 <amagno@xternal.biz>
2021-09-09 16:12:43 -0500

    Initial Commit

---
+++ webapp/controller/DetalleEvento.controller.js
@@ -0,0 +1,21 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(
+	Controller
+) {
+	"use strict";
+
+	return Controller.extend("com.tasa.registroeventospescav2.controller.DetalleEvento", {
+	
+		/**
+		 * @override
+		 */
+		onInit: function() {
+			
+			
+		
+		}
+	
+
+	});
+});
